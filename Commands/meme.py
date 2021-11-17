c3993e30158ee0eb05018559dff2494db770d1bb
RoboAnimal <94212971+RoboAnimal@users.noreply.github.com>
2021-11-17 13:35:31 +0530

    Create meme.py

---
+++ Commands/meme.py
@@ -0,0 +1,9 @@
+@client.command(pass_context=True)
+async def meme(ctx):
+    embed = discord.Embed(title="Memes", description="🤣")
+
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            await ctx.send(embed=embed)
